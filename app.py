880c963d63757086b495edf581aa920d8d33f521
Matiastamayo19 <mtamayog3@eafit.edu.co>
2026-03-12 10:39:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ with st.expander('Analizar texto'):
             with open ('Thumbs up birdie.json') as source:
               animation=json.load(source)
             st.lottie(animation,width =350)
-        elif x >=-1 and x <= 0:
+        elif x >=-1 and x < 0:
             st.write( 'Es un sentimiento Negativo 😔')
             with open ('Sad animation.json') as source:
               animation=json.load(source)
