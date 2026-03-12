dfe242f4ed09c189633237a075dfca48a4855951
Matiastamayo19 <mtamayog3@eafit.edu.co>
2026-03-12 10:19:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from googletrans import Translator
+from streamlit_lottie import st_lottie
 
 st.title('Análisis de Sentimiento')
 image = Image.open('emoticones.jpg')
